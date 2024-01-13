4be5615a5ad2a82e985a520ecf44006a3181c003
Radser2001 <supuni.randeniya@gmail.com>
2024-01-13 14:38:33 +0530

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,44 +3,44 @@ import Image from "next/image";
 import github from "../public/github.png";
 import linkedin from "../public/linkedin.png";
 import mail from "../public/mail.png";
-
+import logo from "../public/logo.svg";
 const Footer = () => {
   return (
     <>
-      <div className="font-mono mt-40">
-        <div className="flex justify-between items-center p-2">
-          <div>
-            <p className="text-md md:text-2xl">
-              Supuni Randeniya | {new Date().getFullYear()}
-            </p>
-          </div>
-          <div className="flex items-center justify-center lg:justify-start">
-            <a
-              href="https://github.com/Radser2001/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image src={github} alt="Github" width={25} height={25} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/supuni-eleesha/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ml-4"
-            >
-              <Image src={linkedin} alt="Linkedin" width={25} height={25} />
-            </a>
-            <a
-              href="mailto:supuni.randeniya@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ml-4"
-            >
-              <Image src={mail} alt="Mail" width={25} height={25} />
-            </a>
-          </div>
-        </div>
-      </div>
+      <footer className="footer mt-20 shadow-xl bg-slate-800 items-center p-4 text-white">
+        <aside className="items-center grid-flow-col">
+          <Image src={logo} width={36} height={36} alt="logo" />
+          <p>
+            Copyright © {new Date().getFullYear()} - Supuni Eleesha Randeniya
+            (Radser2001)
+          </p>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a
+            href="https://github.com/Radser2001/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={github} alt="Github" width={24} height={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/supuni-eleesha/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-4"
+          >
+            <Image src={linkedin} alt="Linkedin" width={24} height={24} />
+          </a>
+          <a
+            href="mailto:supuni.randeniya@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-4"
+          >
+            <Image src={mail} alt="Mail" width={24} height={24} />
+          </a>
+        </nav>
+      </footer>
     </>
   );
 };
