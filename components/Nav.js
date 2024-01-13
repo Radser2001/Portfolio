3b6876c3ed8490deb60109d9572eef8413e21261
Radser2001 <supuni.randeniya@gmail.com>
2024-01-13 09:47:14 +0530

    Update skills section

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,18 @@
+import Link from "next/link";
+
 const Nav = () => {
   return (
     <nav className="sticky top-0 z-50 w-full font-mono flex justify-center  items-center md:justify-end md:items-center p-6 backdrop-blur-md  bg-transparent pr-8 pl-8 ">
       <div className="flex justify-center items-center md:justify-end md:items-center gap-10 md:gap-16">
-        <a className="text-xl md:text-2xl hover:text-sky-500" href="#Home">
-          Home
-        </a>
-        <a className="text-xl md:text-2xl hover:text-sky-500" href="#Skills">
-          Skills
-        </a>
-        <a className="text-xl md:text-2xl hover:text-sky-500" href="#Projects">
-          Projects
-        </a>
+        <Link href="/#Home">
+          <a className="text-xl md:text-2xl hover:text-sky-500">Home</a>
+        </Link>
+        <Link href="/#Skills">
+          <a className="text-xl md:text-2xl hover:text-sky-500">Skills</a>
+        </Link>
+        <Link href="/#Projects">
+          <a className="text-xl md:text-2xl hover:text-sky-500">Projects</a>
+        </Link>
       </div>
     </nav>
   );
